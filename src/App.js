5b3d682e6225579d0a3f135a348b4147eb53c658
borodinka <sofibor03@gmail.com>
2023-05-25 21:07:36 +0200

    Initial styled components setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { ThemeProvider, styled } from "styled-components";
+import { theme } from "./styles/Theme";
+
+const Title = styled.h1`
+  color: ${({ theme }) => theme.colors.primary};
+`;
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Title>Hello</Title>
+    </ThemeProvider>
+  );
 }
 
 export default App;
