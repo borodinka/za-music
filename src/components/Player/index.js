c3afd3d5aa4e49c1160d19f5322675c0c717beaa
borodinka <sofibor03@gmail.com>
2023-06-05 21:33:03 +0200

    Add Reducer & Context

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import Slider from "rc-slider";
 import { ContentWrapper } from "components/Layout";
 import { theme } from "styles/Theme";
@@ -17,69 +17,20 @@ import {
 import IconButton from "components/ui/IconButton";
 import { formatSecondsToMSS } from "utils/time";
 import { Pause, Play, SkipLeft, SkipRight, Volume } from "components/ui/Icons";
-
-const track = {
-  id: 65723649,
-  title: "Another Love",
-  title_short: "Another Love",
-  title_version: "",
-  link: "https://www.deezer.com/track/65723649",
-  duration: 244,
-  rank: 995028,
-  explicit_lyrics: true,
-  explicit_content_lyrics: 0,
-  explicit_content_cover: 0,
-  preview: "https://cdns-preview-5.dzcdn.net/stream/c-59a65b4e38b7911386a2f4adbe24893b-5.mp3",
-  md5_image: "23d34526dc6ef2073bb854fc91a25112",
-  position: 1,
-  artist: {
-    id: 4044787,
-    name: "Tom Odell",
-    link: "https://www.deezer.com/artist/4044787",
-    picture: "https://api.deezer.com/artist/4044787/image",
-    picture_small:
-      "https://e-cdns-images.dzcdn.net/images/artist/b8ac75b41eb8f74a3bef612a5fe8043c/56x56-000000-80-0-0.jpg",
-    picture_medium:
-      "https://e-cdns-images.dzcdn.net/images/artist/b8ac75b41eb8f74a3bef612a5fe8043c/250x250-000000-80-0-0.jpg",
-    picture_big:
-      "https://e-cdns-images.dzcdn.net/images/artist/b8ac75b41eb8f74a3bef612a5fe8043c/500x500-000000-80-0-0.jpg",
-    picture_xl:
-      "https://e-cdns-images.dzcdn.net/images/artist/b8ac75b41eb8f74a3bef612a5fe8043c/1000x1000-000000-80-0-0.jpg",
-    radio: true,
-    tracklist: "https://api.deezer.com/artist/4044787/top?limit=50",
-    type: "artist",
-  },
-  album: {
-    id: 6443018,
-    title: "Long Way Down (Deluxe)",
-    cover: "https://api.deezer.com/album/6443018/image",
-    cover_small:
-      "https://e-cdns-images.dzcdn.net/images/cover/23d34526dc6ef2073bb854fc91a25112/56x56-000000-80-0-0.jpg",
-    cover_medium:
-      "https://e-cdns-images.dzcdn.net/images/cover/23d34526dc6ef2073bb854fc91a25112/250x250-000000-80-0-0.jpg",
-    cover_big:
-      "https://e-cdns-images.dzcdn.net/images/cover/23d34526dc6ef2073bb854fc91a25112/500x500-000000-80-0-0.jpg",
-    cover_xl:
-      "https://e-cdns-images.dzcdn.net/images/cover/23d34526dc6ef2073bb854fc91a25112/1000x1000-000000-80-0-0.jpg",
-    md5_image: "23d34526dc6ef2073bb854fc91a25112",
-    tracklist: "https://api.deezer.com/album/6443018/tracks",
-    type: "album",
-  },
-  type: "track",
-};
+import { PlayerContext, PlayerDispatchContext } from "context/playerContext";
+import { actions } from "context/actions";
 
 function Player() {
+  const dispatch = useContext(PlayerDispatchContext);
+  const { track, isPlaying } = useContext(PlayerContext);
   const [playerState, setPlayerState] = useState({
-    isPlaying: false,
     currentTime: 0,
     duration: 0,
     volume: 0.7,
   });
   const audioRef = useRef();
 
-  const togglePlay = () => {
-    setPlayerState((prev) => ({ ...prev, isPlaying: !prev.isPlaying }));
-  };
+  const togglePlay = () => dispatch({ type: actions.TOGGLE_PLAY });
 
   const toggleVolume = () => {
     const newVolume = playerState.volume > 0 ? 0 : 1;
@@ -114,12 +65,16 @@ function Player() {
   useEffect(() => {
     if (!audioRef?.current) return;
 
-    if (playerState.isPlaying) {
+    if (isPlaying) {
       audioRef.current.play();
     } else {
       audioRef.current.pause();
     }
-  }, [audioRef, track, playerState.isPlaying]);
+  }, [audioRef, track, isPlaying]);
+
+  if (!track) {
+    return null;
+  }
 
   return (
     <Wrapper>
@@ -144,7 +99,7 @@ function Player() {
             <SkipLeft />
           </IconButton>
           <IconButton onClick={togglePlay} width={55} height={55} withBackground>
-            {playerState.isPlaying ? <Pause /> : <Play />}
+            {isPlaying ? <Pause /> : <Play />}
           </IconButton>
           <IconButton>
             <SkipRight />
--- src/components/TracksTable/index.js
+++ src/components/TracksTable/index.js
@@ -3,8 +3,27 @@ import PropTypes from "prop-types";
 import { SubText } from "components/ui/Typography";
 import { Table, TableHead, TableHeading, TableHeadingTime, Line } from "./styled";
 import TrackRow from "./TrackRow";
+import { useContext } from "react";
+import { PlayerContext, PlayerDispatchContext } from "context/playerContext";
+import { actions } from "context/actions";
 
 function TracksTable({ tracks, isLoading }) {
+  const dispatch = useContext(PlayerDispatchContext);
+  const { track, isPlaying } = useContext(PlayerContext);
+
+  const handleTrackClick = (clickedTrack) => {
+    if (track?.id === clickedTrack.id) {
+      dispatch({ type: actions.TOGGLE_PLAY });
+    } else {
+      dispatch({
+        type: actions.SET_TRACKS_DATA,
+        track: clickedTrack,
+        tracks: tracks,
+        isPlaying: true,
+      });
+    }
+  };
+
   return (
     <Table cellSpacing={0}>
       <TableHead>
@@ -31,7 +50,15 @@ function TracksTable({ tracks, isLoading }) {
           <Line colSpan={5} />
         </tr>
         {!isLoading &&
-          tracks?.map((track, index) => <TrackRow key={track.id} track={track} index={index} />)}
+          tracks?.map((currentTrack, index) => (
+            <TrackRow
+              isPlaying={track?.id === currentTrack.id && isPlaying}
+              onClick={handleTrackClick}
+              key={currentTrack.id}
+              track={currentTrack}
+              index={index}
+            />
+          ))}
         {isLoading && [...Array(9).keys()].map((num) => <TrackRow key={num} index={num} />)}
       </tbody>
     </Table>
