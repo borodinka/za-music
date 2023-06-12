337b5f3779f4d226bd3796b18db6cf383139a155
borodinka <sofibor03@gmail.com>
2023-06-12 12:10:50 +0200

    Add play radio functionality

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
-import { loadCharts } from "services/api";
+import { loadCharts, loadTopRadioTracks } from "services/api";
 import { Hero, Genres, Artists } from "components/HomePage";
 import TracksTable from "components/TracksTable";
 import { SectionTitle } from "components/ui/Typography";
@@ -12,14 +12,17 @@ import "swiper/css/pagination";
 
 function Home() {
   const [chart, setChart] = useState();
+  const [radio, setRadio] = useState();
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const loadData = async () => {
       try {
         setIsLoading(true);
-        const data = await loadCharts();
-        setChart(data);
+        const chart = await loadCharts();
+        const radio = await loadTopRadioTracks();
+        setChart(chart);
+        setRadio(radio);
       } catch (err) {
         toast.error(err.message);
       } finally {
@@ -32,7 +35,7 @@ function Home() {
 
   return (
     <main>
-      <Hero />
+      <Hero tracks={radio} />
       <Genres />
       <TrendsAndArtistsSection>
         <div>
